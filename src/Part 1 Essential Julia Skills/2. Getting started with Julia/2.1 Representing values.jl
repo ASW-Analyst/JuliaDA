ce04dffe7c03d305f526f1bbcf61c18d557161f4
ASW-Analyst <andy.wilson8@nhs.net>
2024-02-04 10:36:02 +0000

    Adding checks for data types

--- src/Part 1 Essential Julia Skills/2. Getting started with Julia/2.1 Representing values.jl
+++ src/Part 1 Essential Julia Skills/2. Getting started with Julia/2.1 Representing values.jl
@@ -8,3 +8,24 @@ true
 0.1
 
 [1, 2, 3]
+
+# Checking types of values
+## Using the typeof command
+
+typeof(1)
+
+typeof(true)
+
+typeof("Hello World!")
+
+typeof(0.1)
+
+typeof([1, 2, 3])
+
+# Can check the memory layout of numbers by using the bitstring function
+bitstring(1)
+
+# Can use isa as logical test to check data type
+[1, 2, 3] isa Vector{Int}
+
+true isa String
